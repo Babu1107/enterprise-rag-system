a691620df2bfeeb48a44899ab8d7028c1c0b3fea
Babu Balasubramanian <babu.official@gmail.com>
2026-07-04 13:03:36 +0000

    Add prebuilt FAISS index

--- ingestion.py
+++ ingestion.py
@@ -1,27 +1,18 @@
-from langchain_community.document_loaders import PyPDFLoader
-from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_community.vectorstores import FAISS
-from langchain_community.vectorstores.utils import DistanceStrategy
 from langchain_community.embeddings import HuggingFaceEmbeddings
 
-from config import (
-  PDF_PATH,
-  CHUNK_SIZE,
-  CHUNK_OVERLAP,
-  EMBEDDING_MODEL
+from config import EMBEDDING_MODEL
+
+print("Loading FAISS Index...")
+
+embeddings = HuggingFaceEmbeddings(
+    model_name=EMBEDDING_MODEL
 )
 
-docs = PyPDFLoader(PDF_PATH).load()
-
-splitters = RecursiveCharacterTextSplitter(
-    chunk_size = CHUNK_SIZE,
-    chunk_overlap = CHUNK_OVERLAP
+db = FAISS.load_local(
+    "faiss_index",
+    embeddings,
+    allow_dangerous_deserialization=True
 )
 
-final_docs = splitters.split_documents(docs)
-
-embedding_models = HuggingFaceEmbeddings(
-    model_name = EMBEDDING_MODEL
-)
-
-db = FAISS.from_documents(final_docs, embedding_models, distance_strategy = DistanceStrategy.COSINE)
+print("FAISS Loaded Successfully!")
